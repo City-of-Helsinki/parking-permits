ffda803aa0957eb7763a36bdac1f80b1c92a2769
Sam Hosseini <git-email@hosseini.io>
2021-08-20 13:33:13 +0200

    WIP

--- parking_permits_app/resolvers.py
+++ parking_permits_app/resolvers.py
@@ -1,14 +1,16 @@
-from ariadne import QueryType
+from ariadne import MutationType, QueryType, convert_kwargs_to_snake_case
 from ariadne.contrib.federation import FederatedObjectType
 from django.contrib.auth import get_user_model
 
 from .services.kmo import get_wfs_result, parse_street_name_and_number
 
 query = QueryType()
+mutation = MutationType()
 address_node = FederatedObjectType("AddressNode")
 
 schema_bindables = [
     query,
+    mutation,
     address_node,
 ]
 
@@ -16,6 +18,29 @@ schema_bindables = [
 @query.field("admin_email")
 def resolve_admin_email(*args):
     return get_user_model().objects.get(username="admin").email
+
+
+@mutation.field("createParkingPermit")
+def resolve_create_parking_permit(obj, info, input):
+    return {
+        "parkingPermit": {
+            "identifier": 8000000,
+            "status": input.get("status"),
+            "contractType": "FIXED_PERIOD",
+        }
+    }
+
+
+@mutation.field("updateParkingPermit")
+@convert_kwargs_to_snake_case
+def resolve_update_parking_permit(obj, info, parking_permit, input):
+    return {
+        "parkingPermit": {
+            "identifier": 8000000,
+            "status": input.get("status"),
+            "contractType": "FIXED_PERIOD",
+        }
+    }
 
 
 @address_node.field("locationData")
