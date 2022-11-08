465b7eaa4bbbf323539cd155e3697bb85e562baf
Daniel Prange <prange.daniel@gmail.com>
2022-11-08 14:08:00 +0200

    Make audit log admin read only

--- audit_logger/admin.py
+++ audit_logger/admin.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import json
 import logging
 
 from django.contrib import admin
@@ -13,6 +14,12 @@ class AuditLogAdmin(admin.ModelAdmin):
     list_display_links = ("colored_msg",)
     list_filter = ("level",)
     list_per_page = 10
+    readonly_fields = (
+        "created_at",
+        "level",
+        "is_sent",
+        "message_format",
+    )
 
     def colored_msg(self, instance):
         if instance.level in [logging.NOTSET, logging.INFO]:
@@ -33,6 +40,23 @@ class AuditLogAdmin(admin.ModelAdmin):
         return instance.created_at.strftime("%Y-%m-%d %X")
 
     create_datetime_format.short_description = "Created at"
+
+    def message_format(self, instance):
+        return format_html(
+            "<pre>{msg}</pre>",
+            msg=json.dumps(instance.message, indent=4, sort_keys=True),
+        )
+
+    message_format.short_description = "Message"
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(AuditLog, AuditLogAdmin)
