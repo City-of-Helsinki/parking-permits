aa922eb2ae49313aabbdcc191289c2a7e6dc4066
Daniel Prange <prange.daniel@gmail.com>
2022-11-09 12:29:15 +0200

    Add AuditLoggerAdapter

--- audit_logger/__init__.py
+++ audit_logger/__init__.py
@@ -1,4 +1,5 @@
 from audit_logger import enums  # noqa: F401
+from audit_logger.adapter import AuditLoggerAdapter, getAuditLoggerAdapter  # noqa: F401
 from audit_logger.data import AuditMessage  # noqa: F401
 from audit_logger.db_log_handler import AuditLogHandler  # noqa: F401
 from audit_logger.enums import (  # noqa: F401
--- audit_logger/tests/test_logger.py
+++ audit_logger/tests/test_logger.py
@@ -4,6 +4,7 @@ from django.db import transaction
 from django.test import TestCase
 
 from audit_logger import (
+    AuditLoggerAdapter,
     AuditLogHandler,
     AuditLogLevel,
     AuditMessage,
@@ -13,6 +14,7 @@ from audit_logger import (
     Operation,
     Reason,
     Status,
+    getAuditLoggerAdapter,
 )
 from audit_logger.models import AuditLog
 from audit_logger.tests.utils import make_mock_model
@@ -103,3 +105,44 @@ class AuditLoggerTest(TestCase):
         assert len(AuditLog.objects.all()) == 6
         assert len(AuditLog.objects.filter(message__log_level=AuditLogLevel.ERROR)) == 4
         assert len(AuditLog.objects.filter(level__gte=logging.WARNING)) == 4
+
+    def test_logger_adapter(self):
+        with self.assertLogs("audit_logger", logging.DEBUG) as cm, transaction.atomic():
+            # Setup the logger.
+            logger = logging.getLogger("audit_logger")
+            logger.addHandler(AuditLogHandler())
+            logger.setLevel(logging.DEBUG)
+            adapter = AuditLoggerAdapter(logger, {"origin": "foo"})
+
+            adapter.info(
+                AuditMessage(
+                    "Hello world!",
+                    actor=MockModel(),
+                    target=MockModel(),
+                    operation=Operation.CREATE,
+                    status=Status.SUCCESS,
+                )
+            )
+
+        assert len(cm.records) == 1
+        assert cm.records[0].msg.origin == "foo"
+
+    def test_get_logger_adapter(self):
+        with self.assertLogs("audit_logger", logging.DEBUG) as cm, transaction.atomic():
+            # Setup the logger.
+            adapter = getAuditLoggerAdapter("audit_logger", dict(origin="foo"))
+            adapter.logger.addHandler(AuditLogHandler())
+            adapter.logger.setLevel(logging.DEBUG)
+
+            adapter.info(
+                AuditMessage(
+                    "Hello world!",
+                    actor=MockModel(),
+                    target=MockModel(),
+                    operation=Operation.CREATE,
+                    status=Status.SUCCESS,
+                )
+            )
+
+        assert len(cm.records) == 1
+        assert cm.records[0].msg.origin == "foo"
