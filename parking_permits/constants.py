34ceba5ce21c22c00f27fff894f812a2eae97b64
Daniel Prange <prange.daniel@gmail.com>
2022-11-10 15:14:41 +0200

    Add Origin constants

--- parking_permits/constants.py
+++ parking_permits/constants.py
@@ -6,3 +6,8 @@ class ParkingPermitEndType:
 LOW_EMISSION_DISCOUNT = 50
 SECONDARY_VEHICLE_PRICE_INCREASE = 50
 VAT_PERCENTAGE = 24
+
+
+class Origin:
+    ADMIN_UI = "parking-permits-admin-ui"
+    WEBSHOP = "parking-permits-ui"
