8b302640058f91e02f557af65241abd7e0a92fad
Daniel Prange <prange.daniel@gmail.com>
2022-11-15 08:48:02 +0200

    Add ModelWithId dataclass for audit logging

--- audit_logger/tests/test_data.py
+++ audit_logger/tests/test_data.py
@@ -1,6 +1,12 @@
+import enum
+from json import JSONDecoder
+from unittest import mock
+
 import freezegun
+from django.utils import timezone
 
 from audit_logger import AuditMessage, AuditType, EventType, Operation, Reason, Status
+from audit_logger.data import AuditMessageEncoder, ModelWithId
 from audit_logger.tests.utils import make_mock_model
 
 MockModel = make_mock_model()
@@ -69,3 +75,75 @@ def test_asdict():
 
     # Creation timestamp shouldn't change.
     assert d["date_time"] == msg.date_time
+
+
+@freezegun.freeze_time("2000-01-31 00:01:02.1234567", tz_offset=0)
+@mock.patch(
+    "audit_logger.data.generate_model_id_string_from_instance",
+    return_value="id_from_instance",
+)
+@mock.patch(
+    "audit_logger.data.generate_model_id_string_from_class",
+    return_value="id_from_class",
+)
+def test_json_encoder(mock_gen_id_class, mock_gen_id_instance):
+    class MyEnum(enum.Enum):
+        FOO = "Foo"
+
+    mock_model = MockModel()
+    mock_model_class = mock_model.__class__
+    d = dict(
+        model=mock_model,
+        model_with_id=ModelWithId(mock_model_class, 1),
+        datetime=timezone.now(),
+        enum=MyEnum.FOO,
+    )
+
+    output = JSONDecoder().decode(AuditMessageEncoder().encode(d))
+
+    assert (
+        dict(
+            datetime="2000-01-31T00:01:02.123+00:00",
+            enum="Foo",
+            model="id_from_instance",
+            model_with_id="id_from_class",
+        )
+        == output
+    )
+    mock_gen_id_class.assert_called_once_with(mock_model_class, 1)
+    mock_gen_id_instance.assert_called_once_with(mock_model)
+
+
+@mock.patch(
+    "audit_logger.data.generate_model_id_string_from_instance",
+    return_value="id_from_instance",
+)
+@mock.patch(
+    "audit_logger.data.generate_model_id_string_from_class",
+    return_value="id_from_class",
+)
+def test_json_encoder_with_multiple_models(*_):
+    mock_model = MockModel()
+    mock_model_class = mock_model.__class__
+    d = dict(
+        multiple_models=[
+            MockModel(),
+            MockModel(),
+            ModelWithId(mock_model_class, 100),
+            ModelWithId(mock_model_class, 9000),
+        ]
+    )
+
+    output = JSONDecoder().decode(AuditMessageEncoder().encode(d))
+
+    assert (
+        dict(
+            multiple_models=[
+                "id_from_instance",
+                "id_from_instance",
+                "id_from_class",
+                "id_from_class",
+            ]
+        )
+        == output
+    )
