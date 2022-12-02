5cb2a30595b2fcf187b058471f69a9f4690cb9c0
Daniel Prange <prange.daniel@gmail.com>
2022-12-02 10:59:57 +0200

    Combine address_line related methods

--- parking_permits/management/commands/import_pasi_csv.py
+++ parking_permits/management/commands/import_pasi_csv.py
@@ -85,20 +85,12 @@ class PasiResidentPermit:
     _street_name: typing.Optional[str] = dataclasses.field(init=False, default=None)
     _street_number: typing.Optional[str] = dataclasses.field(init=False, default=None)
 
-    def __setattr__(self, key, value):
-        super(PasiResidentPermit, self).__setattr__(key, value)
-        if key == "address_line":
-            self._compute_street_name_and_number()
-
-    def _compute_street_name_and_number(self):
-        self._street_name, self._street_number = kmo.parse_street_name_and_number(
-            self.address_line
-        )
-
     @address_line.setter
     def address_line(self, val):
         self._address_line = val
-        self._compute_street_name_and_number()
+        self._street_name, self._street_number = kmo.parse_street_name_and_number(
+            self.address_line
+        )
 
     @property
     def street_name(self):
