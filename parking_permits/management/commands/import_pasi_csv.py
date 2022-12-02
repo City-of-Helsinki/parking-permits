79cda162b666bee24b5a7664407e158a4e4879b3
Daniel Prange <prange.daniel@gmail.com>
2022-12-02 10:56:19 +0200

    Add PASI datetime parse

--- parking_permits/management/commands/import_pasi_csv.py
+++ parking_permits/management/commands/import_pasi_csv.py
@@ -1,10 +1,19 @@
+import dataclasses
+import re
 import typing
-from dataclasses import dataclass, field
+import zoneinfo
+from dataclasses import dataclass
 from datetime import datetime
 
 from django.core.management import BaseCommand
+from django.utils import timezone
 
 from parking_permits.services import kmo
+
+# E.g. 1.1.2011 1:01, 31.12.2012 15:50
+PASI_DATETIME_FORMAT = re.compile(
+    r"(?P<day>\d{1,2})\.(?P<month>\d{1,2})\.(?P<year>\d{4})\s*(?P<hour>\d{1,2}):(?P<minute>\d{2})"
+)
 
 
 class Command(BaseCommand):
@@ -12,6 +21,35 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         pass
+
+
+def parse_pasi_datetime(timestamp: str):
+    match = re.match(PASI_DATETIME_FORMAT, timestamp)
+
+    def group_as_int(name: str):
+        return int(match.group(name))
+
+    return timezone.datetime(
+        year=group_as_int("year"),
+        month=group_as_int("month"),
+        day=group_as_int("day"),
+        hour=group_as_int("hour"),
+        minute=group_as_int("minute"),
+        tzinfo=zoneinfo.ZoneInfo("Europe/Helsinki"),
+    )
+
+
+def make_pasi_datetime_property(attr_name):
+    def _get_dt_attr(self) -> datetime:
+        return getattr(self, attr_name)
+
+    def _set_dt_attr(self, val):
+        if isinstance(val, str):
+            setattr(self, attr_name, parse_pasi_datetime(val))
+        else:
+            setattr(self, attr_name, val)
+
+    return property(fget=_get_dt_attr, fset=_set_dt_attr)
 
 
 @dataclass
@@ -34,14 +72,18 @@ class PasiResidentPermit:
     }
 
     id: int
-    start_dt: datetime
-    end_dt: datetime
     national_id_number: str
-    address_line: str
+    city: str
     registration_number: str
+    address_line: str = property(lambda self: self._address_line)
+    start_dt: datetime = make_pasi_datetime_property("_start_dt")
+    end_dt: datetime = make_pasi_datetime_property("_end_dt")
 
-    _street_name: typing.Optional[str] = field(init=False, default=None)
-    _street_number: typing.Optional[str] = field(init=False, default=None)
+    _address_line: str = dataclasses.field(init=False, default=None)
+    _start_dt: datetime = dataclasses.field(init=False, default=None)
+    _end_dt: datetime = dataclasses.field(init=False, default=None)
+    _street_name: typing.Optional[str] = dataclasses.field(init=False, default=None)
+    _street_number: typing.Optional[str] = dataclasses.field(init=False, default=None)
 
     def __setattr__(self, key, value):
         super(PasiResidentPermit, self).__setattr__(key, value)
@@ -52,6 +94,11 @@ class PasiResidentPermit:
         self._street_name, self._street_number = kmo.parse_street_name_and_number(
             self.address_line
         )
+
+    @address_line.setter
+    def address_line(self, val):
+        self._address_line = val
+        self._compute_street_name_and_number()
 
     @property
     def street_name(self):
