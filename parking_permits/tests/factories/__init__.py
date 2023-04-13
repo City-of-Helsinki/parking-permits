d5dcb0c1986c3a4c11c3e936ed43d2356751f8dc
Mika Hietanen <m.hietanen84@gmail.com>
2023-04-13 14:31:31 +0300

    Remove deprecated price test module

--- parking_permits/tests/factories/__init__.py
+++ parking_permits/tests/factories/__init__.py
@@ -1,5 +1,4 @@
-from .price import PriceFactory
 from .vehicle import LowEmissionCriteriaFactory
 from .zone import ParkingZoneFactory
 
-__all__ = ["PriceFactory", "ParkingZoneFactory", "LowEmissionCriteriaFactory"]
+__all__ = ["ParkingZoneFactory", "LowEmissionCriteriaFactory"]
