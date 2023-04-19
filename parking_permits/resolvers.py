211dc4018e10f53c9e4114588ff8f7921c02848f
Mika Hietanen <m.hietanen84@gmail.com>
2023-04-19 17:24:22 +0300

    Adjust address change to always renew order

--- parking_permits/resolvers.py
+++ parking_permits/resolvers.py
@@ -378,53 +378,47 @@ def resolve_update_permit_vehicle(
     new_vehicle.consent_low_emission_accepted = consent_low_emission_accepted
     new_vehicle.save()
 
-    if (
-        permit.contract_type == ContractType.FIXED_PERIOD
-        and permit.vehicle.is_low_emission != new_vehicle.is_low_emission
-    ):
-        price_change_list = permit.get_price_change_list(
-            permit.parking_zone, new_vehicle.is_low_emission
-        )
-        permit_total_price_change = sum(
-            [item["price_change"] * item["month_count"] for item in price_change_list]
-        )
+    price_change_list = permit.get_price_change_list(
+        permit.parking_zone, new_vehicle.is_low_emission
+    )
+    permit_total_price_change = sum(
+        [item["price_change"] * item["month_count"] for item in price_change_list]
+    )
 
-        if permit_total_price_change > 0:
-            permit.next_vehicle = new_vehicle
-            permit.save()
-        else:
-            permit.vehicle = new_vehicle
-            permit.save()
-
-        new_order = Order.objects.create_renewal_order(
-            customer,
-            status=OrderStatus.CONFIRMED,
-            order_type=OrderType.VEHICLE_CHANGED,
-            payment_type=OrderPaymentType.ONLINE_PAYMENT,
-            user=request.user,
-            create_renew_order_event=permit_total_price_change > 0,
-        )
-
-        if permit_total_price_change < 0:
-            refund = Refund.objects.create(
-                name=customer.full_name,
-                order=new_order,
-                amount=-permit_total_price_change,
-                iban=iban if iban else "",
-                description=f"Refund for updating permits, customer switched vehicle to: {new_vehicle}",
-            )
-            logger.info(f"Refund for updating permits created: {refund}")
-            send_refund_email(RefundEmailType.CREATED, customer, refund)
-            ParkingPermitEventFactory.make_create_refund_event(
-                permit, refund, created_by=request.user
-            )
-
-        if permit_total_price_change > 0:
-            checkout_url = TalpaOrderManager.send_to_talpa(new_order)
-            permit.status = ParkingPermitStatus.PAYMENT_IN_PROGRESS
-            talpa_order_created = True
+    if permit_total_price_change > 0:
+        permit.next_vehicle = new_vehicle
+        permit.save()
     else:
         permit.vehicle = new_vehicle
+        permit.save()
+
+    new_order = Order.objects.create_renewal_order(
+        customer,
+        status=OrderStatus.CONFIRMED,
+        order_type=OrderType.VEHICLE_CHANGED,
+        payment_type=OrderPaymentType.ONLINE_PAYMENT,
+        user=request.user,
+        create_renew_order_event=permit_total_price_change > 0,
+    )
+
+    if permit_total_price_change < 0:
+        refund = Refund.objects.create(
+            name=customer.full_name,
+            order=new_order,
+            amount=-permit_total_price_change,
+            iban=iban if iban else "",
+            description=f"Refund for updating permits, customer switched vehicle to: {new_vehicle}",
+        )
+        logger.info(f"Refund for updating permits created: {refund}")
+        send_refund_email(RefundEmailType.CREATED, customer, refund)
+        ParkingPermitEventFactory.make_create_refund_event(
+            permit, refund, created_by=request.user
+        )
+
+    if permit_total_price_change > 0:
+        checkout_url = TalpaOrderManager.send_to_talpa(new_order)
+        permit.status = ParkingPermitStatus.PAYMENT_IN_PROGRESS
+        talpa_order_created = True
 
     permit.vehicle_changed = False
     permit.vehicle_changed_date = None
