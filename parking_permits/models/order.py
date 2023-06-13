f91d39159e757e578fc149386a669ae7ba81534f
Mika Hietanen <m.hietanen84@gmail.com>
2023-06-13 12:47:54 +0300

    Simplify Order cancel-method

--- parking_permits/models/order.py
+++ parking_permits/models/order.py
@@ -369,18 +369,18 @@ class Order(SerializableMixin, TimestampedModelMixin, UserStampedModelMixin):
     def total_payment_price_vat(self):
         return sum([item.total_payment_price_vat for item in self.order_items.all()])
 
-    def _cancel_talpa_order(self, talpa_order_id):
+    def _cancel_talpa_order(self):
         headers = {
             "api-key": settings.TALPA_API_KEY,
             "namespace": settings.NAMESPACE,
             "Content-Type": "application/json",
         }
         response = requests.post(
-            f"{settings.TALPA_ORDER_EXPERIENCE_API}/{talpa_order_id}/cancel",
+            f"{settings.TALPA_ORDER_EXPERIENCE_API}/{self.talpa_order_id}/cancel",
             headers=headers,
         )
         if response.status_code == 200:
-            logger.info(f"Talpa order cancelling successful: {talpa_order_id}")
+            logger.info(f"Talpa order cancelling successful: {self.talpa_order_id}")
             return True
         else:
             logger.error("Talpa order cancelling failed")
@@ -395,26 +395,23 @@ class Order(SerializableMixin, TimestampedModelMixin, UserStampedModelMixin):
             )
 
     @transaction.atomic
-    def cancel(self, talpa_order_id=None, cancel_from_talpa=True):
-        logger.info(f"Cancelling order: {talpa_order_id}")
-        order = Order.objects.get(talpa_order_id=talpa_order_id)
-
+    def cancel(self, cancel_from_talpa=True):
+        logger.info(f"Cancelling order: {self.talpa_order_id}")
         try:
-            OrderValidator.validate_order(talpa_order_id, order.customer.user.uuid)
+            OrderValidator.validate_order(self.talpa_order_id, self.customer.user.uuid)
         except OrderValidationError as e:
             logger.error(f"Order validation failed. Error = {e}")
             return False
-
         # Try to cancel order from Talpa as well
         if cancel_from_talpa:
             try:
-                self._cancel_talpa_order(talpa_order_id)
+                self._cancel_talpa_order()
             except OrderCancelError:
                 logger.warning(
                     "Talpa order cancelling failed. Continuing the cancel process.."
                 )
-        order.status = OrderStatus.CANCELLED
-        order.save()
+        self.status = OrderStatus.CANCELLED
+        self.save()
         return True
 
 
@@ -513,9 +510,7 @@ class Subscription(SerializableMixin, TimestampedModelMixin, UserStampedModelMix
         )
         # Mark the order as cancelled if it has no active subscriptions left
         if not remaining_valid_order_subscriptions.exists():
-            order.cancel(
-                talpa_order_id=talpa_order_id, cancel_from_talpa=cancel_from_talpa
-            )
+            order.cancel(cancel_from_talpa=cancel_from_talpa)
 
         # Create a refund for a remaining full month period, if it was charged already
         if permit.end_time and permit.end_time - relativedelta(months=1) > tz.now():
