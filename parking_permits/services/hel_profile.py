62361ffd9c71a5f52b35c144594649b977ce9cab
Mika Hietanen <m.hietanen84@gmail.com>
2023-10-05 15:59:42 +0300

    Get user names from profile verified personal info

--- parking_permits/services/hel_profile.py
+++ parking_permits/services/hel_profile.py
@@ -30,16 +30,20 @@ class HelsinkiProfile:
 
         email_node = self.__profile.get("primaryEmail")
         phone_node = self.__profile.get("primaryPhone")
+        verified_info = self.__profile.get("verifiedPersonalInformation")
+        national_id_number = (
+            verified_info.get("nationalIdentificationNumber") if verified_info else None
+        )
+        first_name = verified_info.get("firstName") if verified_info else None
+        last_name = verified_info.get("lastName") if verified_info else None
         return {
             "source_system": SourceSystem.HELSINKI_PROFILE,
             "source_id": self.__profile.get("id"),
-            "first_name": self.__profile.get("firstName"),
-            "last_name": self.__profile.get("lastName"),
+            "first_name": first_name,
+            "last_name": last_name,
             "email": email_node.get("email") if email_node else None,
             "phone_number": phone_node.get("phone") if phone_node else None,
-            "national_id_number": self.__profile.get(
-                "verifiedPersonalInformation", {}
-            ).get("nationalIdentificationNumber"),
+            "national_id_number": national_id_number,
         }
 
     def _get_profile(self):
