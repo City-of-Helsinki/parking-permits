d3e41263dbb0e204ce1faea5c69ba84b0417783b
Mika Hietanen <m.hietanen84@gmail.com>
2023-11-09 19:02:09 +0200

    Close non-valid permits automatically

--- parking_permits/tests/test_cron.py
+++ parking_permits/tests/test_cron.py
@@ -19,40 +19,141 @@ from parking_permits.tests.factories.parking_permit import ParkingPermitFactory
 
 class CronTestCase(TestCase):
     def setUp(self):
-        self.customer = CustomerFactory(first_name="Firstname A", last_name="")
-        ParkingPermitFactory(
-            customer=self.customer,
-            end_time=datetime(2023, 3, 31, tzinfo=dt_tz.utc),
-            status=ParkingPermitStatus.VALID,
-        )
-        ParkingPermitFactory(
-            customer=self.customer,
-            end_time=datetime(2023, 3, 29, tzinfo=dt_tz.utc),
-            status=ParkingPermitStatus.DRAFT,
-        )
-        ParkingPermitFactory(
-            customer=self.customer,
-            end_time=datetime(2023, 3, 31, tzinfo=dt_tz.utc),
-            status=ParkingPermitStatus.DRAFT,
-        )
-        ParkingPermitFactory(
-            customer=self.customer,
-            end_time=datetime(2023, 3, 29, tzinfo=dt_tz.utc),
-            status=ParkingPermitStatus.VALID,
-        )
+        self.customer = CustomerFactory(first_name="Stephen", last_name="Strange")
 
-    @freeze_time(tz.make_aware(datetime(2023, 3, 30)))
-    def test_automatic_expiration_of_older_permits(self):
+    @freeze_time(tz.make_aware(datetime(2023, 11, 30)))
+    def test_automatic_expiration_permits(self):
+        ParkingPermitFactory(
+            customer=self.customer,
+            end_time=datetime(2023, 12, 1, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.VALID,
+        )
+        ParkingPermitFactory(
+            customer=self.customer,
+            end_time=datetime(2023, 11, 29, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.DRAFT,
+        )
+        ParkingPermitFactory(
+            customer=self.customer,
+            end_time=datetime(2023, 12, 1, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.DRAFT,
+        )
+        ParkingPermitFactory(
+            customer=self.customer,
+            end_time=datetime(2023, 11, 29, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.VALID,
+        )
+        ParkingPermitFactory(
+            customer=self.customer,
+            end_time=datetime(2023, 11, 29, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.CLOSED,
+        )
         valid_permits = ParkingPermit.objects.filter(status=ParkingPermitStatus.VALID)
         draft_permits = ParkingPermit.objects.filter(status=ParkingPermitStatus.DRAFT)
         self.assertEqual(valid_permits.count(), 2)
         self.assertEqual(draft_permits.count(), 2)
-
         automatic_expiration_of_permits()
         closed_permits = ParkingPermit.objects.filter(status=ParkingPermitStatus.CLOSED)
         self.assertEqual(valid_permits.count(), 1)
+        self.assertEqual(draft_permits.count(), 0)
+        self.assertEqual(closed_permits.count(), 2)
+
+    @freeze_time(tz.make_aware(datetime(2023, 11, 30)))
+    def test_automatic_expiration_permits_with_primary_permit_change(self):
+        ParkingPermitFactory(
+            id=80000010,
+            customer=self.customer,
+            end_time=datetime(2023, 11, 29, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.VALID,
+            primary_vehicle=True,
+        )
+        ParkingPermitFactory(
+            id=80000020,
+            customer=self.customer,
+            end_time=datetime(2023, 12, 1, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.VALID,
+            primary_vehicle=False,
+        )
+        ParkingPermitFactory(
+            customer=self.customer,
+            end_time=datetime(2023, 11, 29, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.DRAFT,
+        )
+        ParkingPermitFactory(
+            customer=self.customer,
+            end_time=datetime(2023, 12, 1, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.DRAFT,
+        )
+        valid_permits = ParkingPermit.objects.filter(status=ParkingPermitStatus.VALID)
+        draft_permits = ParkingPermit.objects.filter(status=ParkingPermitStatus.DRAFT)
+        closed_permits = ParkingPermit.objects.filter(status=ParkingPermitStatus.CLOSED)
+        self.assertEqual(valid_permits.count(), 2)
         self.assertEqual(draft_permits.count(), 2)
+        self.assertEqual(closed_permits.count(), 0)
+        automatic_expiration_of_permits()
+        self.assertEqual(valid_permits.all().count(), 1)
+        valid_permit = valid_permits.first()
+        self.assertEqual(valid_permit.id, 80000020)
+        self.assertEqual(valid_permit.primary_vehicle, True)
+        self.assertEqual(draft_permits.all().count(), 0)
+        closed_permits = closed_permits.all()
         self.assertEqual(closed_permits.count(), 1)
+        closed_permit = closed_permits.first()
+        self.assertEqual(closed_permit.id, 80000010)
+        self.assertEqual(
+            closed_permit.end_time,
+            datetime(2023, 11, 29, 23, 59, 59, 999999, tzinfo=dt_tz.utc),
+        )
+
+    @freeze_time(tz.make_aware(datetime(2023, 11, 30)))
+    def test_automatic_expiration_permits_with_permit_vehicle_changed(self):
+        ParkingPermitFactory(
+            id=80000010,
+            customer=self.customer,
+            end_time=datetime(2023, 12, 29, tzinfo=dt_tz.utc),
+            vehicle_changed=True,
+            vehicle_changed_date=datetime(2023, 11, 29, tzinfo=dt_tz.utc).date(),
+            status=ParkingPermitStatus.VALID,
+            primary_vehicle=True,
+        )
+        ParkingPermitFactory(
+            id=80000020,
+            customer=self.customer,
+            end_time=datetime(2023, 12, 29, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.VALID,
+            primary_vehicle=False,
+        )
+        ParkingPermitFactory(
+            customer=self.customer,
+            end_time=datetime(2023, 11, 29, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.DRAFT,
+        )
+        ParkingPermitFactory(
+            customer=self.customer,
+            end_time=datetime(2023, 12, 1, tzinfo=dt_tz.utc),
+            status=ParkingPermitStatus.DRAFT,
+        )
+        valid_permits = ParkingPermit.objects.filter(status=ParkingPermitStatus.VALID)
+        draft_permits = ParkingPermit.objects.filter(status=ParkingPermitStatus.DRAFT)
+        closed_permits = ParkingPermit.objects.filter(status=ParkingPermitStatus.CLOSED)
+        self.assertEqual(valid_permits.count(), 2)
+        self.assertEqual(draft_permits.count(), 2)
+        self.assertEqual(closed_permits.count(), 0)
+        automatic_expiration_of_permits()
+        valid_permits = valid_permits.all()
+        self.assertEqual(valid_permits.count(), 1)
+        valid_permit = valid_permits.first()
+        self.assertEqual(valid_permit.id, 80000020)
+        self.assertEqual(valid_permit.primary_vehicle, True)
+        self.assertEqual(draft_permits.all().count(), 0)
+        closed_permits = closed_permits.all()
+        self.assertEqual(closed_permits.count(), 1)
+        closed_permit = closed_permits.first()
+        self.assertEqual(closed_permit.id, 80000010)
+        self.assertEqual(
+            closed_permit.end_time,
+            datetime(2023, 11, 29, 23, 59, 59, 999999, tzinfo=dt_tz.utc),
+        )
 
 
 class AutomaticRemoveObsoleteCustomerDataTestCase(TestCase):
