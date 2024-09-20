ca6b84742050d457605d8e051d83ef3d667b4681
Toni Pellinen <toni.pellinen@anders.com>
2024-09-20 15:43:51 +0300

    Switch email backend to django-mailer
    Refs: PV-872

--- project/settings.py
+++ project/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = [
     "drf_yasg",
     "django_crontab",
     "encrypted_fields",
+    "mailer",
 ]
 
 MIDDLEWARE = [
@@ -304,6 +305,7 @@ EMAIL_PORT = env.int("EMAIL_PORT")
 EMAIL_TIMEOUT = env.int("EMAIL_TIMEOUT")
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 THIRD_PARTY_PARKING_PROVIDER_EMAILS = env("THIRD_PARTY_PARKING_PROVIDER_EMAILS")
+EMAIL_BACKEND = "mailer.backend.DbBackend"
 
 sentry_sdk.init(
     dsn=env.str("SENTRY_DSN"),
