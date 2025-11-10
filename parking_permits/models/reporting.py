f2e404e05c322c3c7d6018b8a364dd3fa9992a26
Vesa-Matti Virolainen <vesa-matti.virolainen@anders.com>
2025-11-10 10:57:35 +0200

    feat: calculate daily permit count snapshots
    Implement logic for calculating permit count snapshots for the current
    date.
    
    refs PV-966

--- parking_permits/models/reporting.py
+++ parking_permits/models/reporting.py
@@ -1,7 +1,14 @@
-from django.db import models
+import datetime
+
+from django.db import models, transaction
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-from parking_permits.models.parking_permit import ContractType
+from parking_permits.models.parking_permit import (
+    ContractType,
+    ParkingPermit,
+    ParkingPermitStatus,
+)
 
 
 class PermitCountSnapshot(models.Model):
@@ -43,3 +50,119 @@ class PermitCountSnapshot(models.Model):
                 name="unique_daily_snapshot",
             )
         ]
+
+    @staticmethod
+    def get_counts_for_permit_ids(
+        *, permit_ids, low_emission: bool, date: datetime.date
+    ):
+        grouping_fields = (
+            "parking_zone__name",
+            "parking_zone__description",
+            "parking_zone__description_sv",
+            "primary_vehicle",
+            "contract_type",
+        )
+
+        permit_count_data = (
+            ParkingPermit.objects.filter(id__in=permit_ids)
+            .select_related("parking_zone")
+            # IMPORTANT:
+            # - values() is used to group the data by the relevant
+            # fields
+            # - values() _MUST_ be called before annotating the permit
+            # count into the queryset as otherwise were just annotating
+            # a count of 1 into all the entries without doing any
+            # proper grouping.
+            # - We're always running this logic for a given emission
+            # type and date, these are annotated in.
+            .values(*grouping_fields)
+            .annotate(
+                permit_count=models.Count("id"),
+                low_emission=models.Value(low_emission),
+                date=models.Value(date),
+            )
+        )
+
+        return permit_count_data
+
+    @staticmethod
+    def build_daily_snapshot():
+        now = timezone.now()
+        now_date = now.date()
+
+        # Get permits that are going to contribute to the
+        # daily snapshot
+        valid_permits = ParkingPermit.objects.filter(
+            status=ParkingPermitStatus.VALID,
+            start_time__lte=now,
+            end_time__gte=now,
+        ).select_related("vehicle__power_type")
+
+        # Split the permits into lists of permit ids by their
+        # low-emission-status, this allows us to calculate the grouped
+        # counts on the database-level.
+        valid_low_emission_permit_ids = []
+        valid_high_emission_permit_ids = []
+        for permit in valid_permits:
+            if permit.vehicle.is_low_emission:
+                valid_low_emission_permit_ids.append(permit.id)
+            else:
+                valid_high_emission_permit_ids.append(permit.id)
+
+        low_emission_permit_counts = PermitCountSnapshot.get_counts_for_permit_ids(
+            permit_ids=valid_low_emission_permit_ids,
+            low_emission=True,
+            date=now_date,
+        )
+
+        high_emission_permit_counts = PermitCountSnapshot.get_counts_for_permit_ids(
+            permit_ids=valid_high_emission_permit_ids,
+            low_emission=False,
+            date=now_date,
+        )
+
+        permit_counts = list(low_emission_permit_counts) + list(
+            high_emission_permit_counts
+        )
+
+        permit_counts_to_create = []
+        permit_counts_to_update = []
+
+        for count_entry in permit_counts:
+            filter_dict = {
+                "date": count_entry["date"],
+                "parking_zone_name": count_entry["parking_zone__name"],
+                "parking_zone_description": count_entry["parking_zone__description"],
+                "parking_zone_description_sv": count_entry[
+                    "parking_zone__description_sv"
+                ],
+                "low_emission": count_entry["low_emission"],
+                "primary_vehicle": count_entry["primary_vehicle"],
+                "contract_type": count_entry["contract_type"],
+            }
+
+            permit_count_snapshot = PermitCountSnapshot(
+                **filter_dict,
+                permit_count=count_entry["permit_count"],
+            )
+
+            if pre_existing_snapshot := PermitCountSnapshot.objects.filter(
+                **filter_dict
+            ).first():
+                permit_count_snapshot.id = pre_existing_snapshot.id
+                permit_counts_to_update.append(permit_count_snapshot)
+            else:
+                permit_counts_to_create.append(permit_count_snapshot)
+
+        with transaction.atomic():
+            fields_to_update = ("permit_count",)
+            PermitCountSnapshot.objects.bulk_update(
+                permit_counts_to_update,
+                fields=fields_to_update,
+                batch_size=5000,
+            )
+
+            PermitCountSnapshot.objects.bulk_create(
+                permit_counts_to_create,
+                batch_size=5000,
+            )
---
+++ parking_permits/tests/test_reporting.py
@@ -0,0 +1,156 @@
+import itertools
+import math
+from datetime import date, datetime, time
+
+from dateutil.relativedelta import relativedelta
+from django.db import IntegrityError
+from django.test import TestCase
+from django.utils import timezone
+from freezegun import freeze_time
+
+from parking_permits.models.parking_permit import ContractType, ParkingPermitStatus
+from parking_permits.models.reporting import PermitCountSnapshot
+from parking_permits.tests.factories.parking_permit import ParkingPermitFactory
+from parking_permits.tests.factories.vehicle import (
+    VehicleFactory,
+    VehiclePowerTypeFactory,
+)
+from parking_permits.tests.factories.zone import ParkingZoneFactory
+
+
+class PermitCountSnapshotTestCase(TestCase):
+
+    def test_creating_duplicate_permit_count_snapshot_raises(self):
+
+        test_date = date(2024, 11, 30)
+        def create_snapshot(*, count: int):
+            PermitCountSnapshot.objects.create(
+                permit_count=count,
+                date=test_date,
+                parking_zone_name="A",
+                parking_zone_description="Kallio",
+                parking_zone_description_sv="Berghäll",
+                low_emission=True,
+                primary_vehicle=True,
+                contract_type=ContractType.OPEN_ENDED,
+            )
+
+        create_snapshot(count=0)
+        with self.assertRaises(IntegrityError):
+            create_snapshot(count=2)
+
+    def test_build_daily_permit_count_snapshot(self):
+
+        test_date = date(2024, 11, 30)
+        # First day of the previous month from test_date
+        permit_start_date = (test_date - relativedelta(months=2)) + relativedelta(
+            days=1
+        )
+        # Last day of the next month from test_date
+        permit_end_date = test_date + relativedelta(months=1)
+        permit_start_time = datetime.combine(permit_start_date, time(0, 0, 0))
+        permit_end_time = datetime.combine(permit_end_date, time(23, 59, 59))
+
+        zone_a = ParkingZoneFactory(
+            name="A",
+            description="Kallio",
+            description_sv="Berghäll",
+        )
+        zone_b = ParkingZoneFactory(
+            name="B",
+            description="Etu-Töölö",
+            description_sv="Främre Tölö",
+        )
+
+        # NOTE: identifier="04" makes this grant low-emission-status
+        # for the vehicle
+        electric_power_type = VehiclePowerTypeFactory(identifier="04")
+        non_electric_power_type = VehiclePowerTypeFactory(identifier="01")
+        low_emission_vehicle = VehicleFactory(power_type=electric_power_type)
+        high_emission_vehicle = VehicleFactory(power_type=non_electric_power_type)
+
+        # generate a different count of permits for all the different dimension value combinations:
+        parking_zones = (
+            zone_a,
+            zone_b,
+        )
+        vehicles = (
+            low_emission_vehicle,
+            high_emission_vehicle,
+        )
+        contract_types = (
+            ContractType.FIXED_PERIOD,
+            ContractType.OPEN_ENDED,
+        )
+        vehicle_primarity = (
+            True,
+            False,
+        )
+
+        dimension_lists = (
+            parking_zones,
+            vehicles,
+            contract_types,
+            vehicle_primarity,
+        )
+
+        combination_count = math.prod(len(dim_list) for dim_list in dimension_lists)
+        for count_to_create, dimensions in enumerate(
+            itertools.product(*dimension_lists), start=1
+        ):
+            for _ in range(count_to_create):
+                ParkingPermitFactory(
+                    status=ParkingPermitStatus.VALID,
+                    start_time=permit_start_time,
+                    end_time=permit_end_time,
+                    parking_zone=dimensions[0],
+                    vehicle=dimensions[1],
+                    contract_type=dimensions[2],
+                    primary_vehicle=dimensions[3],
+                )
+
+        # Run the snapshot-building logic on test_date
+        with freeze_time(
+            timezone.make_aware(
+                datetime(test_date.year, test_date.month, test_date.day, 22)
+            )
+        ):
+            PermitCountSnapshot.build_daily_snapshot()
+
+            now = timezone.now()
+            now_date = now.date()
+
+        permit_count_data = PermitCountSnapshot.objects.filter(date=now_date)
+        self.assertEqual(len(permit_count_data), combination_count)
+
+        # Check that a snapshot exists for all the expected
+        # permit count values. (from 1 to the total amount of dimension
+        # combinations, inclusive from both ends.)
+        # NOTE: we're relying heavily here on the unique-constraint on
+        # the PermitCountSnapshot-model as it guarantees that the
+        # different counts correspond to different dimension value
+        # combinations.
+        for target_count in range(1, combination_count + 1):
+            self.assertTrue(
+                permit_count_data.filter(permit_count=target_count).exists()
+            )
+
+        # Run the snapshot-building logic on test_date _AGAIN_
+        # to force updates over creates
+        with freeze_time(
+            timezone.make_aware(
+                datetime(test_date.year, test_date.month, test_date.day, 22)
+            )
+        ):
+            PermitCountSnapshot.build_daily_snapshot()
+
+        # Re-fetch data after running the updates on the snapshots
+        permit_count_data = PermitCountSnapshot.objects.filter(date=now_date)
+
+        # Previus assertions should still hold as we're just updating
+        # the daily counts with unchanged permit data
+        self.assertEqual(len(permit_count_data), combination_count)
+        for target_count in range(1, combination_count + 1):
+            self.assertTrue(
+                permit_count_data.filter(permit_count=target_count).exists()
+            )
