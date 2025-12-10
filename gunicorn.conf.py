6c6ea2757861a9cb2ac24ce018038447897c8dfd
Mika Hietanen <m.hietanen84@gmail.com>
2025-12-10 17:18:27 +0200

    feat: change gunicorn logging format to json
    Refs: PV-979

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,54 @@
 # https://docs.gunicorn.org/en/stable/settings.html
+from logger_extra.extras.gunicorn import JsonErrorFormatter, JsonFormatter
 
 wsgi_app = "project.wsgi"
 bind = "0.0.0.0:8888"
 limit_request_field_size = 65536
 
-accesslog = "-"  # '-' makes gunicorn log to stdout
-errorlog = "-"  # '-' makes gunicorn log to stderr
+logconfig_dict = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "context": {
+            "()": "logger_extra.filter.LoggerContextFilter",
+        }
+    },
+    "formatters": {
+        "json": {
+            "()": JsonFormatter,
+        },
+        "json_error": {
+            "()": JsonErrorFormatter,
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "json",
+            "filters": ["context"],
+            "stream": "ext://sys.stdout",
+        },
+        "error_console": {
+            "class": "logging.StreamHandler",
+            "formatter": "json_error",
+            "filters": ["context"],
+            "stream": "ext://sys.stderr",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "gunicorn.access": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "gunicorn.error": {
+            "level": "INFO",
+            "handlers": ["error_console"],
+            "propagate": False,
+        },
+    },
+}
