22dc67ee1e9b1173bc6bdd30d98692214add3a20
Mika Hietanen <m.hietanen84@gmail.com>
2026-09-02 15:30:58 +0300

    fix: force test environment for resilient logger config
    django-resilient-logger requires RESILIENT_LOGGER['environment']
    to be a non-empty string. Locally this depended on AUDIT_LOG_ENV,
    which was unset, so the test suite failed outside of CI. Override
    'environment' explicitly in settings_test so tests no longer rely
    on that variable being configured.
    
    Refs: SHPPWS-268

--- project/settings_test.py
+++ project/settings_test.py
@@ -4,6 +4,11 @@ FIELD_ENCRYPTION_KEYS = [
     "f164ec6bd6fbc4aef5647abc15199da0f9badcc1d2127bde2087ae0d794a9a0b"
 ]
 
+# The `environment` value is required (non-empty) by django-resilient-logger's
+# config validation. Make sure it's always set in tests regardless of what
+# (if anything) is configured for AUDIT_LOG_ENV in the local environment/.env.
+RESILIENT_LOGGER = {**RESILIENT_LOGGER, "environment": "test"}  # noqa: F405
+
 # Some of our tests check for error messages, in English,
 # so we don't want them to be translated to, for example, Finnish.
 LANGUAGE_CODE = "en"
